91933fe4286439502e4ba69c91ca10274f974ffc
cristhianA94 <cristhianapolo.94@gmail.om>
2020-06-02 04:58:16 -0500

    Service notificaciones

---
+++ src/app/core/services/notificaciones/alerts.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertsService {
+
+  constructor(private toastr: ToastrService) { }
+
+  mensajeExito(titulo, mensaje) {
+    this.toastr.success(mensaje, titulo);
+  }
+  mensajeError(titulo, mensaje){
+    this.toastr.error(mensaje, titulo);
+  }
+}
