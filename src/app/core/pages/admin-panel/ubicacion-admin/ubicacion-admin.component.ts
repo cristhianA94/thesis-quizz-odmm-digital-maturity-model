f16e0fb43e03227033cad70f7c6e751fc8ecf464
cristhianA94 <cristhianapolo.94@gmail.om>
2020-06-04 00:12:31 -0500

    Add ubicaciones admin component

---
+++ src/app/core/pages/admin-panel/ubicacion-admin/ubicacion-admin.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-ubicacion-admin',
+  templateUrl: './ubicacion-admin.component.html',
+  styleUrls: ['./ubicacion-admin.component.css']
+})
+export class UbicacionAdminComponent implements OnInit {
+
+
+  constructor(
+
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+
+
+}
+
