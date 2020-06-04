d0bd45c1fc8814fb32bbc7f5e8f93a749af2977f
cristhianA94 <cristhianapolo.94@gmail.om>
2020-06-04 00:11:29 -0500

    Add provincia admin

---
+++ src/app/core/pages/admin-panel/ubicacion-admin/admin-provincia/admin-provincia.component.ts
@@ -0,0 +1,129 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+// Material
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatAccordion } from '@angular/material/expansion';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogFormProvinciaComponent } from './dialog-form-provincia.component';
+
+import { Pais } from '../../../../../shared/models/ubicacion';
+import { ProvinciaService } from '../../../../services/user/provincia/provincia.service';
+import { Provincia } from 'app/shared/models/ubicacion';
+import { PaisService } from 'app/core/services/user/pais/pais.service';
+
+@Component({
+  selector: 'app-admin-provincia',
+  templateUrl: './admin-provincia.component.html',
+  styleUrls: ['./admin-provincia.component.css']
+})
+export class AdminProvinciaComponent implements OnInit {
+
+
+  @ViewChild(MatAccordion) paisesItems: MatAccordion;
+
+  provincias: Provincia[] = [];
+  provincia: Provincia;
+
+  displayedColumns: string[] = ['Nombre', ' '];
+
+  dataSource = new MatTableDataSource();
+
+  //@ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(
+    public paisService: PaisService,
+    public provinciaService: ProvinciaService,
+    public dialog: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+    this.provinciaService.getProvinciasDB().subscribe(provincias => {
+      this.provincias = provincias;
+      /* Table */
+      // Agrega los datos a la tabla
+      this.dataSource.data = this.provincias;
+      //this.paisService.getPaisDB(this.provincias[].id)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+
+    /* 
+    this.provincias.forEach(element => {
+      console.log(element);
+      this.paisService.getPaisDB(element.idPais).subscribe( pais=>{
+        console.log(pais);
+      }) 
+    }); */
+
+  }
+
+  // Table
+  buscarProvincia(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onRowClicked(row: any) {
+    this.provincia = row;
+    //console.log('Row clicked: ', this.provincias);
+  }
+
+  openDialog(action: string, obj: any) {
+
+    // Agregar la accion al objeto para mostrarla en el dialog
+    const dialogRef = this.dialog.open(DialogFormProvinciaComponent, {
+      width: '300px',
+      data: {
+        obj,
+        action: action
+      }
+    });
+
+    // Despues de cerrar el dialog
+    dialogRef.afterClosed().subscribe(result => {
+      // Si esta vacion no retorna nada
+      if (!result) {
+        return;
+      }
+
+      this.provincia = result.data;
+      // Si NO es nuevo se le agrega el id
+      if (result.id) {
+        this.provincia.id = result.id;
+      }
+
+      if (result.event == 'Agregar') {
+        this.createProvincia(this.provincia);
+      } else if (result.event == 'Actualizar') {
+        this.updateProvincia(this.provincia);
+      } else if (result.event == 'Borrar') {
+        this.deleteProvincia(this.provincia);
+      }
+
+    });
+  }
+
+  createProvincia(provincia: Provincia) {
+    this.provinciaService.createProvinciaDB(provincia);
+  }
+
+  updateProvincia(provincia: Provincia) {
+    this.provinciaService.updateProvinciaDB(provincia);
+  }
+
+  deleteProvincia(provincia: Provincia) {
+    this.provinciaService.deleteProvinciaDB(provincia);
+  }
+
+}
---
+++ src/app/core/pages/admin-panel/ubicacion-admin/admin-provincia/dialog-form-provincia.component.ts
@@ -0,0 +1,60 @@
+import { Component, Inject, Optional, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PaisService } from '../../../../services/user/pais/pais.service';
+import { Pais } from '../../../../../shared/models/ubicacion';
+
+@Component({
+  selector: 'app-dialog-form-provincia',
+  templateUrl: './dialog-form-provincia.component.html',
+  styles: [
+    `
+    .dialog-actions button{
+      margin: 7px;
+      display: flex;
+    }
+    `
+  ]
+})
+export class DialogFormProvinciaComponent {
+
+  paises: Pais[] = [];
+  dataForm: FormGroup;
+  action: string;
+  local_data: any;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogFormProvinciaComponent>,
+    public paisService: PaisService,
+    public _formBuilder: FormBuilder,
+    //@Optional() is used to prevent error if no data is passed
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
+    // Carga los paises
+    this.paisService.getPaisesDB().subscribe(paises => {
+      this.paises = paises;
+    });
+    // Agrega el objeto a un objeto local
+    this.local_data = data.obj;
+    // Separa la accion del objeto (Agregar, actualizar o borrar)
+    this.action = data.action;
+
+    this.dataForm = this.buildForm();
+  }
+
+
+  doAction() {
+    // Manda el tipo de accion que se hizo (Agregar, Actualizar o Borrar) y los datos del formulario y el id
+    this.dialogRef.close({ event: this.action, data: this.dataForm.value, id: this.local_data.id });
+  }
+
+  closeDialog() {
+    this.dialogRef.close({ event: 'Cancelar' });
+  }
+
+  buildForm(): FormGroup {
+    return this._formBuilder.group({
+      nombre: [this.local_data.nombre, Validators.required],
+      idPais: [null]
+    });
+  }
+}
