8e5443b992a068b22260e8b7bac5271858578a0b
cristhianA94 <cristhianapolo.94@gmail.om>
2020-07-15 15:14:25 -0500

    Protect Keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAQ37PQ4mi1ZpAGSqUn0_3La1lxNzXW1i4",
-    authDomain: "fir-auth-web-75274.firebaseapp.com",
-    databaseURL: "https://fir-auth-web-75274.firebaseio.com",
-    projectId: "fir-auth-web-75274",
-    storageBucket: "fir-auth-web-75274.appspot.com",
-    messagingSenderId: "10805534927",
-    appId: "1:10805534927:web:43d05b00ff7672b4",
-    measurementId: "G-P50JCF26VS"
+    // API KEYS Firebase here
   }
 };
