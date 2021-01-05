ea635387a455523176e0d789f37a3b9bdfa43629
cristhianA94 <cristhianapolo.94@gmail.om>
2021-01-04 21:27:21 -0500

    remove keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBzOutMPBAjGxEzYoUn6z5SxgJMhrN4exU",
-    authDomain: "odmm-autodiagostico.firebaseapp.com",
-    projectId: "odmm-autodiagostico",
-    storageBucket: "odmm-autodiagostico.appspot.com",
-    messagingSenderId: "865588017822",
-    appId: "1:865588017822:web:f7227796559ce85ab2d98f",
-    measurementId: "G-8KYSCF5Z1N"
+    /* PRIVATE KEY */
   }
 };
